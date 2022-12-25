3f91db3c514ca663cbad806fa7a63cd608bc2c0e
Rostyslav Onikiienko <r.onikienko@gmail.com>
2022-12-25 21:26:24 +0200

    fix: more fixes in localization

--- src/stats/Components/FilteredStats/ListItem.js
+++ src/stats/Components/FilteredStats/ListItem.js
@@ -11,22 +11,23 @@ export const ListItem = ({listItem, index}) => {
             <div className="list-item-block-details">
                 <span className="list-item-index">{index + 1}.) </span>
                 <span
-                    title="Коли відео було аналізовано">{getReadableDate(listItem[CHECKED_VIDEOS_DB_KEYS.timeWhenBlocked])}</span>
+                    title={chrome.i18n.getMessage('list_item_time_analyzed_help')}>{getReadableDate(listItem[CHECKED_VIDEOS_DB_KEYS.timeWhenBlocked])}</span>
                 <span
-                    title="Причина та деталі детекту (якщо результат гугл то буде написано мову яку гугл задетектив, якщо слова, то слово яке було знайдено)"
+                    title={chrome.i18n.getMessage('list_item_detect_reason_help')}
                     className="list-item-block-reason"
                 >
                     {listItem[CHECKED_VIDEOS_DB_KEYS.reason]}&nbsp;
                     {listItem?.[CHECKED_VIDEOS_DB_KEYS.reasonDetails] ? `(${listItem?.[CHECKED_VIDEOS_DB_KEYS.reasonDetails]})` : ''}
                 </span>
-                <span title="ID відео" className="list-item-yt-id">{listItem[CHECKED_VIDEOS_DB_KEYS.ytId]}</span>
+                <span title={chrome.i18n.getMessage('list_item_video_id_help')}
+                      className="list-item-yt-id">{listItem[CHECKED_VIDEOS_DB_KEYS.ytId]}</span>
             </div>
             <Link
                 href={listItem[CHECKED_VIDEOS_DB_KEYS.link]}
                 text={listItem[CHECKED_VIDEOS_DB_KEYS.title]}
             />
             <span className="list-item-channel-name">
-                - {listItem[CHECKED_VIDEOS_DB_KEYS.channelName] || 'Short video'}
+                - {listItem[CHECKED_VIDEOS_DB_KEYS.channelName] || chrome.i18n.getMessage('list_item_short_video')}
             </span>
         </div>
     );
