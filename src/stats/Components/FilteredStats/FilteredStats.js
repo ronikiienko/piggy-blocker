910dcd813b3bd75d2b11c5d655323d9322e9a7e6
Rostyslav Onikiienko <r.onikienko@gmail.com>
2022-12-26 03:08:24 +0200

    update TODOS

--- src/stats/Components/FilteredStats/FilteredStats.js
+++ src/stats/Components/FilteredStats/FilteredStats.js
@@ -91,6 +91,9 @@ export const FilteredStats = () => {
         [BLOCK_REASONS_MAP.google]: 0,
     });
     // TODO +1 rerender happens somehow
+    // TODO percents wrong
+    // TODO sometimes videos are sent twice to backend
+    // TODO reverse list
     React.useEffect(() => {
         let byCharsTitle = 0;
         let noCyrillic = 0;
